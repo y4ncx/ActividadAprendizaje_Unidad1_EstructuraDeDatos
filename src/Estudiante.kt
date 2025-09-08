78e0f5c3bf2461839580c9c67ffb52e2e33879d2
y4ncx <y4ncxs@gmail.com>
2025-09-08 17:51:56 -0500

    Se agrega el segundo ejercicio resuelto

--- src/Estudiante.kt
+++ src/Estudiante.kt
@@ -7,4 +7,16 @@ data class Estudiante(
 
 fun main(args: Array<String>) {
 
+    //CREAMOS 3 INSTANCIAS DE NUESTRA CLASE ESTUDIANTE
+    val estudiane1 = Estudiante("Juan", 18, 4.5)
+    val estudiante2 = Estudiante("Linda", 20, 5.0)
+    val estudiante3 = Estudiante("JhonDoe", 19, 3.4)
+
+    //LAS GUARDAMOS EN UN ARREGLO
+    val estudiantes = arrayOf(estudiane1, estudiante2, estudiante3)
+
+
+    //IMPRIMIMOS LA INFORMACION POR INDICE
+    println(estudiantes[0].nombre)
+    
 }
