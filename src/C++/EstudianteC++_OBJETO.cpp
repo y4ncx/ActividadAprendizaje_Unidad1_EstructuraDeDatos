be9457e8eb6727a3436e5e1772e138a1ccfd39fb
saidm <smartelod@unicartagena.edu.co>
2025-09-12 13:57:10 -0500

    Cambios (abrir): -Nuevo metodo de mostrarInformacion -Setter para cambiar el promedio -Se crearon los objetos ficticios -La creacion del arreglo para cada estudiante

--- src/C++/EstudianteC++_OBJETO.cpp
+++ src/C++/EstudianteC++_OBJETO.cpp
@@ -17,4 +17,30 @@ public:
         edad = e;
         promedio = p;
     }
+
+    // se crea el metodo para mostrar la informacion
+    void mostrarInformacion()
+    {
+        cout << "Nombre: " << nombre << endl;
+        cout << "Edad: " << edad << endl;
+        cout << "Promedio: " << promedio << endl;
+    }
+
+    // se crea el metodo setter para modificar el promedio
+    void setPromedio(double promedioNuevo)
+    {
+        promedio = promedioNuevo;
+    }
 };
+
+int main()
+{
+    // se crean las 3 objeto de la clase Estudiante
+    Estudiante estudiante1("Jane Doe", 20, 8.5);
+    Estudiante estudiante2("Pablito Calvito", 22, 9.2);
+    Estudiante estudiante3("Said", 18, 7.8);
+
+    // se crea un arreglo para cada estudiante
+    Estudiante estudiantes [3] = {estudiante1, estudiante2, estudiante3};
+    return 0;
+}
