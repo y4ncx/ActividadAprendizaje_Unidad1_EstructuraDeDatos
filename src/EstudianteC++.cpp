096e12abbdaf91bd99a4f00674abcae256f7859e
saidm <smartelod@unicartagena.edu.co>
2025-09-12 13:20:46 -0500

    Se reestructuro la funcion principal para mejor lectura, tambien se agrego el arreglo de estudiante y el recorrido del arreglo

--- src/EstudianteC++.cpp
+++ src/EstudianteC++.cpp
@@ -2,17 +2,30 @@
 
 using namespace std;
 
-struct Estudiante {
+struct Estudiante
+{
     string nombre;
     int edad;
     double promedio;
 };
 
-main(){
-// Se crean las 3 instancias de Estudiante
-Estudiante estudiante1 = {"Alfredo", 20, 8.5};
-Estudiante estudiante2 = {"Marcelo", 22, 9.0};
-Estudiante estudiante3 = {"James", 19, 9.5};
+main()
+{
+    // Se crean las 3 instancias de Estudiante
+    Estudiante estudiante1 = {"Alfredo", 20, 8.5};
+    Estudiante estudiante2 = {"Marcelo", 22, 9.0};
+    Estudiante estudiante3 = {"James", 19, 9.5};
 
-    return 0;
+    // Se crea un arreglo de Estudiante
+    Estudiante estudiantes[3] = {estudiante1, estudiante2, estudiante3};
+
+    // El recorrido de cada estudiante en el arreglo
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Nombre: " << estudiantes[i].nombre << endl;
+        cout << "Edad: " << estudiantes[i].edad << endl;
+        cout << "Promedio: " << estudiantes[i].promedio << endl;
+
+        return 0;
+    }
 }
