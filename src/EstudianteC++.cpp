102b69a316c3bae8b5cf8a584d98fa484da55283
saidm <smartelod@unicartagena.edu.co>
2025-09-12 13:14:17 -0500

    Se agregan las 3 instancias en EstudianteC++.cpp con valores ficticios.

--- src/EstudianteC++.cpp
+++ src/EstudianteC++.cpp
@@ -9,7 +9,10 @@ struct Estudiante {
 };
 
 main(){
-
+// Se crean las 3 instancias de Estudiante
+Estudiante estudiante1 = {"Alfredo", 20, 8.5};
+Estudiante estudiante2 = {"Marcelo", 22, 9.0};
+Estudiante estudiante3 = {"James", 19, 9.5};
 
     return 0;
 }
